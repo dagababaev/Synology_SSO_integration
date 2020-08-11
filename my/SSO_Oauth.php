ce7eb115d46dc00473eae9776d33a8feefa22964
Dmitri Agababaev <66842523+dagababaev@users.noreply.github.com>
2020-08-12 02:10:48 +0300

    Update SSO_Oauth.php

--- my/SSO_Oauth.php
+++ my/SSO_Oauth.php
@@ -1,4 +1,15 @@
 <?php
+
+// ------------------------------------------------------------------------------
+//  © Copyright (с) 2020
+//  Author: Dmitri Agababaev, d.agababaev@duncat.net
+//
+//  Redistributions and use of source code, with or without modification, are
+//  permitted that retain the above copyright notice
+//
+//  License: MIT
+// ------------------------------------------------------------------------------
+
 session_start();
 
 include_once('config.php');
