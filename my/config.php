608565a2959e520f2bb8bac0bf842c85954766b5
Dmitri Agababaev <66842523+dagababaev@users.noreply.github.com>
2020-08-12 02:11:10 +0300

    Update config.php

--- my/config.php
+++ my/config.php
@@ -1,5 +1,15 @@
 <?php
 
+// ------------------------------------------------------------------------------
+//  © Copyright (с) 2020
+//  Author: Dmitri Agababaev, d.agababaev@duncat.net
+//
+//  Redistributions and use of source code, with or without modification, are
+//  permitted that retain the above copyright notice
+//
+//  License: MIT
+// ------------------------------------------------------------------------------
+
 define('APP_ID', 'a8d0f0835eda3517f3e8fd70c10500e7');
 define('SSO_HOST', 'https://DSM:5001');
 define('LOCAL_HOST', 'https://yourwebsite.ru');
