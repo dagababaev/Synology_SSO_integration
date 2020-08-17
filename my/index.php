78c85f75d1b096aab8ba4fa0dca73c7dabbf04d2
Dmitri Agababaev <66842523+dagababaev@users.noreply.github.com>
2020-08-17 12:39:53 +0300

    Update index.php

--- my/index.php
+++ my/index.php
@@ -18,7 +18,7 @@ if (!isset($_SESSION['user_id'])) {
   header('location: '.SSO_HOST.'/webman/sso/SSOOauth.cgi?app_id='.APP_ID.'&scope=user_id&redirect_uri='.REDIRECT_URI);
 }
 
-if (isset($_GET['logout']) && $_GET['logout'] == 'true') {
+if (isset($_GET['logout'])) {
   unset($_SESSION['user_id']);
   header('location: '.LOCAL_HOST);
 }
